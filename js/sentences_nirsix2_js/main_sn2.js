c4e70d315322216e0d63a46a3c66cfdeac9a8c4f
nirslife <111741149+nirslife@users.noreply.github.com>
2022-10-01 11:32:30 +0300

    Create main_sn2.js

---
+++ js/sentences_nirsix2_js/main_sn2.js
@@ -0,0 +1,104 @@
+
+let vGV; // глобальная переменная - она будет одна
+
+function MainDBForFunction() {
+  // тут БД всех функций приложения, где и в каком файле они находятся
+  let main_sn2 = {
+    jsname:"main_sn2.js",
+    fa:
+    [
+      {namefun:"Main_App_Run"},
+      {namefun:"Main_Init_VarObj_Start"},
+      {namefun:"Create_HTML_BODY_Start"}      
+    ]
+  }  
+
+  let htmlblock_sn2 = {
+    jsname:"htmlblock_sn2.js",
+    fa:
+    [
+      {func:"createitemctrlbutton"},
+      {func:"Create_HeaderBlock"},
+      {func:"Create_FooterBlock"},
+      {func:"clickButtondownctrlblock"},
+      {func:"clickClearInput"},
+      {func:"clickLessonNum"},
+      {func:"NextSentenceOnClick"},
+      {func:"TestSentOnClick"},
+      {func:"clickInputtext"},
+      {func:"clickPuzzletext"},
+      {func:"FormArr_puzzletextfrom"}         
+    ]
+  }  
+
+  let object_sn2 = {
+    jsname:"object_sn2.js",
+    fa:
+    [
+      {func:"Get_HtmlBodyObj"}
+    ]
+  }  
+
+  let insets_sn2 = {
+    jsname:"insets_sn2.js",
+    fa:
+    [
+      {func:"Init_LoginFireBaseSets"},
+      {func:"Init_Eventvalue_Devices"},
+      {func:"LoadSentencesJson"},
+      {func:"Init_SpeechSynthesisUtterance"},
+      {func:"Init_GlobalVar"},
+      {func:"Get_GlobalVar"},
+      {func:"LoginFireBase"},
+      {func:"CallBackLoginFireBase"},
+
+      {func:"RequestArrFireBase"},
+      {func:"LoginFireBase"},
+      {func:"LoginFireBase"},
+    ]
+  }
+
+  let utils_sn2 = {
+    jsname:"utils_sn2.js",
+    fa:
+    [
+    {func:"SliceSentence"},
+    {func:"MixArray"},
+    {func:"getRandomInt"},    
+    {func:"InitRandomArr"},
+    {func:"InitRandomArr3"}    
+    ]
+  }
+  
+}
+
+ 
+function Main_Init_VarObj_Start(gv) {
+  gv.eventvalue = Init_Eventvalue_Devices();
+  gv.HtmlBodyObj = Get_HtmlBodyObj();  
+  gv.msgspeak = Init_SpeechSynthesisUtterance();   
+  gv.FBSets = Init_LoginFireBaseSets();
+  gv.DefStepMix = 5;
+  gv.CurSentences = 0;
+  gv.LessonNum = 3;
+  //инит gv.CountLessonNum перенесен в function CallBackGetLesson(gv, vdata)   
+  gv.pvoiceitemctrl = null; 
+  gv.funCBAfterLoadArrLesson = DisplayExercise;
+}
+
+function Create_HTML_BODY_Start(gv) {  
+  gv.HtmlBodyObj.createThis();
+  Create_HeaderBlock(gv);
+  Create_FooterBlock(gv);  
+  LoginFireBase(gv);
+}
+
+function Main_App_Run() {
+  vGV = Init_GlobalVar();
+  Main_Init_VarObj_Start(vGV);  
+  Create_HTML_BODY_Start(vGV);  
+}  
+
+
+
+  
