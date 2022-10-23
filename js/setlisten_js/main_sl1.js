549199ea3752aaa5fb4789811979c6f651234af7
nirslife <111741149+nirslife@users.noreply.github.com>
2022-10-24 01:11:08 +0300

    Update main_sl1.js

--- js/setlisten_js/main_sl1.js
+++ js/setlisten_js/main_sl1.js
@@ -2,6 +2,7 @@ let vGV; // глобальная переменная - она будет одн
 
 
 function Main_Init_VarObj_Start(gv) {
+  gv.ProgName = "Voice"; //gv.ProgName = "Sentence";
   gv.eventvalue = Init_Eventvalue_Devices();
   gv.HtmlBodyObj = Get_HtmlBodyElem();
   gv.HtmlAnyObj = Get_HtmlAnyElem(gv);
@@ -9,6 +10,8 @@ function Main_Init_VarObj_Start(gv) {
   gv.FBSets = Init_LoginFireBaseSets();
   gv.HtmlBV = Get_HtmlBV(gv);
   gv.HtmlTst = Get_HtmlTst(gv); // для тестов   
+  gv.MMenu =  Get_HtmlMMenu(gv); // main menu
+  gv.LstLes =  Get_HtmlLstLes(gv); // список уроков
   gv.funInit_LessonVarObj = Init_LessonVarObj;
   gv.ArVP = Get_ArVP(gv);
 //  gv.funInit_BlViVarObj = Init_BlViVarObj;
@@ -18,6 +21,12 @@ function Main_Init_VarObj_Start(gv) {
   gv.funCBAfterLoadArrLesson = AfterLoadArrLesson;
   gv.funCBBeforeLoadAfterPatch = BeforeLoadAfterPatch;
 }
+
+function AfterLoadArrLesson(gv) {  
+  LoadlessTo_mmenu(gv);
+  gv.ArVP.ar = FormVoiceArrCurSentence(gv);
+}
+
 
 // просто для примера
 function BeforeLoadAfterPatch(gv, vdata) {
@@ -36,10 +45,13 @@ function Create_HTML_BODY_Start(gv) {
 //!!!не удалять!!!
 */
 
+
+
 // инспытательный вариант Create_HTML_BODY_Start
 function Create_HTML_BODY_Start(gv) {
   gv.HtmlBodyObj.createThis_AddChilds();
-  gv.funCBAfterLoadArrLesson = TempAfterLoadArrLesson; // пустую функцию, пока ничего делать не нужно
+  create1mmenu_obj(gv);
+  //gv.funCBAfterLoadArrLesson = TempAfterLoadArrLesson; // пустую функцию, пока ничего делать не нужно
   //create1Blocktest1(gv);
   create1BlockForVoice1(gv);
   LoginFireBase(gv);
