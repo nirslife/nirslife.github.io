99922b0e4046eaf61e87dd07371266018e6cd65d
nirslife <111741149+nirslife@users.noreply.github.com>
2022-11-08 23:26:43 +0200

    Update htmlblock_sn2.js

--- js/sentences_nirsix2_js/htmlblock_sn2.js
+++ js/sentences_nirsix2_js/htmlblock_sn2.js
@@ -30,9 +30,9 @@ function Create_FooterBlock(gv) {
   Butt2.setAttributeNode(att2);
     
   let m_cobj = gv.HtmlBodyObj.kp["m_container1"]; //GetpObj("m_container1");  
-  let AnyObj = gv.HtmlAnyObj; 
+  let AnyObj = gv.HtmlAnyObj;
   m_cobj.appendChild(Butt1);
-  m_cobj.appendChild(Butt2);     
+  m_cobj.appendChild(Butt2);
   m_cobj.appendChild(AnyObj.createElemByInx("StepMixS_1","1"));
   m_cobj.appendChild(AnyObj.createElemByInx("StepMixS_2","2"));
   m_cobj.appendChild(AnyObj.createElemByInx("StepMixS_4","4"));
@@ -52,7 +52,7 @@ function clickButtondownctrlblock(athis) {
 }
 */
 
-function clickClearInput(aThis){
+function clickClearInput(aThis) {
   InpNode = document.getElementById("div_inputtextfrom1");
   if (InpNode) {
     while (InpNode.firstChild) {
@@ -83,12 +83,13 @@ function NextSentenceOnClick() {
   SendToBDCurSentences(gv);
   Check_DisplayExercise(gv);
   const remNode = document.getElementById("div_verify_cont1"); 
-  if (remNode){remNode.remove();}  
+  if (remNode){remNode.remove();}
   document.getElementById("verify_cont1").className = "verify_def";  
 }
 
 
 function CheckInputText(gv) {
+  TestWordCheckInputText(gv);
   if (TestCheckInputText(gv)){
     document.getElementById("inputtextfrom1").style.background = "rgb(76, 175, 80)";
   }else{
@@ -97,12 +98,12 @@ function CheckInputText(gv) {
 }
 
 function TestCheckInputText(gv) {
-  let inpstr = ""; 
+  let inpstr = "";
   let srcstr = "";
   const myCollection = document.getElementsByClassName("inputtext");
   for (let i = 0; i < myCollection.length; i++) {
     //myCollection[i].style.color = "red";
-    inpstr = inpstr + myCollection[i].innerHTML;
+    inpstr = inpstr + myCollection[i].innerText;
   }  
   inpstr = inpstr.replaceAll(" ","");
   inpstr = inpstr.toUpperCase();
@@ -110,12 +111,50 @@ function TestCheckInputText(gv) {
   srcstr = srcstr.toUpperCase();
   gv.sinpstr = inpstr;
   gv.ssrcstr = srcstr;
-  if (inpstr == srcstr){ 
+  if (inpstr == srcstr){
     return true;
   }else{
     return false;
   }
 }
+
+function TestWordCheckInputText(gv) {
+  let arp = [];
+  let lastix = -1;
+  let its_ok = 1;
+  let SrcEng = SliceSentence(gv.ArrSens[gv.CurSentences].Eng);
+  const myCollection = document.getElementsByClassName("inputtext");
+  let len = myCollection.length;
+  if (len > SrcEng.length) {len = SrcEng.length;}
+  for (let i = 0; i < len; i++) {
+    myCollection[i].style.color = "rgb(0, 0, 0)";
+    let it = {po:null, wr:"", ok:0, end:0};
+    it.po = myCollection[i];
+    it.wr = myCollection[i].innerText;
+    let s1 = SrcEng[i].toUpperCase();
+    let s2 = it.wr.toUpperCase();
+    s1 = s1.replaceAll(" ","");
+    s2 = s2.replaceAll(" ","");
+    if (s1 == s2){
+      it.ok = 1;
+    }else{
+      it.ok = 0;
+      its_ok = 0;
+    }
+    if ((it.wr.endsWith("."))||(it.wr.endsWith("!"))||it.wr.endsWith("?")){
+      it.end = 1;
+      if (its_ok) {lastix = i;}
+    }else{
+      it.end = 0;
+    }
+    arp.push(it);
+  }
+  for (let i = 0; i <= lastix; i++) {
+    myCollection[i].style.color = "rgb(90, 165, 102)";
+  }
+
+}
+
 
 function TestSentOnClick() {
   let gv = Get_GlobalVar();
@@ -150,50 +189,50 @@ function TestSentOnClick() {
   TestCheckInputText(gv);
   inpstr = gv.sinpstr;
   srcstr = gv.ssrcstr;
-  const EngSrc = document.createElement("div");  
+  const EngSrc = document.createElement("div");
   EngSrc.className = "verify_info";
   EngSrc.innerText = srcstr;
-  const EngInp = document.createElement("div");  
+  const EngInp = document.createElement("div");
   EngInp.className = "verify_info";
   EngInp.innerText = inpstr;
   if (inpstr == srcstr){
-    vrfNode.className = "verify_green";   
-    bInfo.innerText = "ХОРОШО!";  	  
+    vrfNode.className = "verify_green";
+    bInfo.innerText = "ХОРОШО!";
   }
   else{
-    vrfNode.className = "verify_red"; 
-    bInfo.innerText = "НЕ ВЕРНО!";  	  
+    vrfNode.className = "verify_red";
+    bInfo.innerText = "НЕ ВЕРНО!";
   }  
-  rootDiv.appendChild(EngSrcSen);  
+  rootDiv.appendChild(EngSrcSen);
   rootDiv.appendChild(EngSrc);
-  rootDiv.appendChild(EngInp);    
-  vrfNode.appendChild(rootDiv);      
+  rootDiv.appendChild(EngInp);
+  vrfNode.appendChild(rootDiv);
 }
     
 function clickInputtext(aThis) {
-  let gv = Get_GlobalVar();  
-  const InpBl = document.getElementById("Puzzid"+aThis.getAttribute("IndexArr"));  
+  let gv = Get_GlobalVar();
+  const InpBl = document.getElementById("Puzzid"+aThis.getAttribute("IndexArr"));
   InpBl.className = "puzzleblock inpon";
-  const att1 = document.createAttribute(gv.eventvalue);    
-  att1.value = "clickPuzzletext(this)";  
+  const att1 = document.createAttribute(gv.eventvalue);
+  att1.value = "clickPuzzletext(this)";
   InpBl.setAttributeNode(att1);
   aThis.remove();
   CheckInputText(gv);
 }
 
 function clickPuzzletext(aThis) {
-  let gv = Get_GlobalVar();  
+  let gv = Get_GlobalVar();
   aThis.removeAttribute(gv.eventvalue);
-  aThis.className = "inpoff puzzleblock";    
+  aThis.className = "inpoff puzzleblock";
   const InTxt = document.createElement("div");
   InTxt.innerText = aThis.innerText;
-  InTxt.className = "inputtext";      
-  const att1 = document.createAttribute(gv.eventvalue);//   
-  att1.value = "clickInputtext(this)";  
-  InTxt.setAttributeNode(att1);             
+  InTxt.className = "inputtext";
+  const att1 = document.createAttribute(gv.eventvalue);//
+  att1.value = "clickInputtext(this)";
+  InTxt.setAttributeNode(att1);
   const att2 = document.createAttribute("IndexArr");
-  att2.value = aThis.getAttribute("IndexArr");  
-  InTxt.setAttributeNode(att2);       
+  att2.value = aThis.getAttribute("IndexArr");
+  InTxt.setAttributeNode(att2);
   speak(gv, aThis.innerText, 1.2);
   document.getElementById("div_inputtextfrom1").appendChild(InTxt);
   CheckInputText(gv);
@@ -207,7 +246,7 @@ function FormArr_puzzletextfrom(gv){
   Indx = gv.CurSentences;
   // IndxNei - индекс предложения для подмешивания слов
   let IndxNei = Indx + 1;
-  if (IndxNei >= obj.length){IndxNei = 0;}   
+  if (IndxNei >= obj.length){IndxNei = 0;}
   let EngArrDbl = SliceSentence(obj[Indx].Eng); // разбивка англ. варианта пары
   let EngArrNei = SliceSentence(obj[IndxNei].Eng); // разбивка англ. из соседней пары для подмешивания 
     
@@ -241,13 +280,14 @@ function DisplayExercise(gv) {
   let StepMix = gv.DefStepMix;
   let mm1 = gv.MMenu;
   if (obj[Indx].StepMix) { 
-    StepMix = obj[Indx].StepMix;    
+    StepMix = obj[Indx].StepMix;
   }else{
     obj[Indx].StepMix = StepMix;
     SendToBDArrSens(gv);
   }
 
   mm1.op["StepMix1"].innerText = StepMix;
+  SetActiveStepMix(gv);
 
   CheckInputText(gv); //вместо document.getElementById("--------").style.background = "rgb(255, 255, 255)";
   // очищаем старое, если есть  
@@ -282,7 +322,7 @@ function DisplayExercise(gv) {
   textR.innerText = obj[Indx].Rus; 
   const att3 = document.createAttribute("onmousedown"); //onclick //ontouchstart
   att3.value = "clickTextfrom(this)";  
-  textR.setAttributeNode(att3);         
+  textR.setAttributeNode(att3);
   
   //document.getElementById("footerinfcount").innerText = Indx + " of " + obj.length;
   document.getElementById("nextsentbutt").innerText = Indx + " of " + obj.length;
@@ -290,7 +330,7 @@ function DisplayExercise(gv) {
 }   // end of function
   
 function clickTextfrom(aThis) {
-  let gv = Get_GlobalVar();    
+  let gv = Get_GlobalVar();
   let inpstr = ""; 
   const myCollection = document.getElementsByClassName("inputtext");
   for (let i = 0; i < myCollection.length; i++) {    
@@ -302,11 +342,11 @@ function clickTextfrom(aThis) {
 function clickStepMixChange(athis) {
   let e1 = 0;    
   if (athis.id == "StepMixDec1") {e1 = -1;}
-  if (athis.id == "StepMixInc1") {e1 = 1;}    
-  let gv = Get_GlobalVar();        
+  if (athis.id == "StepMixInc1") {e1 = 1;}
+  let gv = Get_GlobalVar();
   let maxm = gv.MaxStepMix / 1;
   obj = gv.ArrSens; Indx = gv.CurSentences; 
-  let aValue = obj[Indx].StepMix;   
+  let aValue = obj[Indx].StepMix; 
   if (aValue) {       
     aValue = aValue + e1;
     if (aValue == 0) {aValue = 1;}
@@ -318,7 +358,7 @@ function clickStepMixChange(athis) {
     Check_DisplayExercise(gv);
   }
   SetActiveStepMix(gv);
-}    
+}
 
 function clickStepMixSet(athis) {
   let gv = Get_GlobalVar();
@@ -327,7 +367,7 @@ function clickStepMixSet(athis) {
   if (athis.id == "StepMixS_1") {aValue = 1;}
   if (athis.id == "StepMixS_2") {aValue = 2;}
   if (athis.id == "StepMixS_4") {aValue = 4;}
-  if (athis.id == "StepMixS_7") {aValue = 7;}  
+  if (athis.id == "StepMixS_7") {aValue = 7;}
   if (aValue != obj[Indx].StepMix) {
     obj[Indx].StepMix = aValue;
     SendToBDArrSens(gv); // сохраняем урок в базу (хотя нужно сохранить только StepMix)
@@ -336,12 +376,12 @@ function clickStepMixSet(athis) {
   SetActiveStepMix(gv);
 }
 
-function SetActiveStepMix(gv) {  
+function SetActiveStepMix(gv) {
   document.getElementById("StepMixS_1").style.background = "rgb(30, 144, 255)";
   document.getElementById("StepMixS_2").style.background = "rgb(30, 144, 255)";
   document.getElementById("StepMixS_4").style.background = "rgb(30, 144, 255)";
   document.getElementById("StepMixS_7").style.background = "rgb(30, 144, 255)";
-  obj = gv.ArrSens;
+  obj = gv.ArrSens; Indx = gv.CurSentences;
   let aval = 0;
   switch(obj[Indx].StepMix) {
     case 1:
