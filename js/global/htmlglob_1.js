e1c323b401ec0a842c9b2450ce51b73da77216c8
nirslife <111741149+nirslife@users.noreply.github.com>
2023-04-28 22:12:53 +0300

    Update htmlglob_1.js

--- js/global/htmlglob_1.js
+++ js/global/htmlglob_1.js
@@ -2,12 +2,19 @@ function create1BlockMenu3(gv) {
   let mm1 = gv.MMenu;
   let bd2 = mm1.op["mm_div3_i"];
   let txtb = "";
+  let txt2 = "";
   if (gv.OffLineMode == 1){
     txtb = "Off line";
   }else{
     txtb = "ONLINE";
   }
+  if (gv.RusTalkMode == 1){
+    txt2 = "Rus_ON";
+  }else{
+    txt2 = "Rus_Off";
+  }
   bd2.appendChild(mm1.create1ElemByInx("mm_offlinemode_i",txtb));
+  bd2.appendChild(mm1.create1ElemByInx("mm_RusTalkMode_i",txt2));
 }
 
 function click_mm_offlinemode_i(athis){
@@ -21,5 +28,20 @@ function click_mm_offlinemode_i(athis){
     gv.OffLineMode = 1;
     gv.MMenu.op["mm_offlinemode_i"].innerText = "Off line";
     RequestOffLineBase(gv);
+  }
+}
+
+function click_mm_RusTalkMode_i(athis){
+  let gv = Get_GlobalVar();
+  if (gv.RusTalkMode == 1){
+    gv.RusTalkMode = 0;
+    gv.MMenu.op["mm_RusTalkMode_i"].innerText = "Rus_Off";
+    gv.RIF = "";
+    gv.ArVP.ar = FormVoiceArrCurSentence(gv);
+  }else{
+    gv.RusTalkMode = 1;
+    gv.MMenu.op["mm_RusTalkMode_i"].innerText = "Rus_ON";
+    gv.RIF = "R";
+    gv.ArVP.ar = FormVoiceArrCurSentence(gv);
   }
 }
