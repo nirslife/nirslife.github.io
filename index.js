0515de8912f52da27d04e0066667cf4d16501384
nirslife <nice.days.life@gmail.com>
2024-10-11 01:23:44 +0300

    3

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 //import * as localBD from 'testmod.js';
+import {GetLocalBD} from './js/setlisten_js/localBD.js'; 
 import {sayHi, sayBye} from './testmod.js';
 
 
 function Main_App_Run(arg1) {    
-
-    sayHi('John'); // Hello, John!
-    sayBye('John'); // Bye, John!
+    let vs = GetLocalBD();
+    console.log(vs);
+   // sayHi('John'); // Hello, John!
+   // sayBye('John'); // Bye, John!
 }
 
 export default Main_App_Run;
